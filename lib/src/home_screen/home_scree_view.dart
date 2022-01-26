e422dbce4946094033c1e5fd90f06f63f75fb1a7
Hooshyar <hooseyr@gmail.com>
2022-01-26 19:21:43 +0300

    init

---
+++ lib/src/home_screen/home_scree_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenView extends StatefulWidget {
+  const HomeScreenView({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenViewState createState() => _HomeScreenViewState();
+}
+
+class _HomeScreenViewState extends State<HomeScreenView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
